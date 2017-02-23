6208b48c110cc446b504ab746482c8724580fdab
Killian Davitt <davittk@tcd.ie>
2017-02-23 17:27:43 +0000

    Changed fyp.el to use xelatex

--- fyp.el
+++ fyp.el
@@ -1,10 +1,2 @@
 (require 'ox-latex) 
-(add-to-list 'org-latex-classes
-             '("fyp"
-               "\\documentclass{article}"
-               ("\\chapter{%s}" . "\\chapter*{%s}")
-	       ("\\section{%s}" . "\\chapter*{%s}")
-	       ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(setq org-latex-compiler "xelatex")
